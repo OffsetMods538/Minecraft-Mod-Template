0ed69d3871a14893761cadc83f2fd92e29be0963
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-12-23 21:51:18 +0200

    Make outlet automatically update gradle.properties. Can be disabled by setting the "DISABLE_PROPERTIES_UPDATE" environment variable.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -57,6 +57,7 @@ shutil.rmtree("resources")
 
 
 os.system("gradle wrapper --gradle-version=8.5 --gradle-distribution-sha256-sum=9d926787066a081739e8200858338b4a69e837c3a821a33aca9db09dd4a41026")
+os.system("gradle outletPropertiesUpdate")
 os.system("gradle genSources")
 
 
